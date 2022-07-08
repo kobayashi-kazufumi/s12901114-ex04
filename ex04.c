f5ab0844fadf82db4b46e4c4cf47f83ee784480e
kabayashi_kazufumi <kobayashi_kazufumi@plise.co.jp>
2022-07-08 16:44:30 +0900

    HoT: initial revision

---
+++ ex04.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int h=0;
+    int t=0;
+    char name[30];
+    int p;
+    
+
+
+    printf("Tosshing coin...\n");
+    
+    for(int i=0;i<3;i++){
+         p=rand()%2;
+        if(p==1){
+            printf("Round%d Head\n",i+1);
+            h++;
+        }else {
+            t++;
+            printf("Round%d Tail\n",i+1);
+    
+        }
+
+    }
+
+    printf("Head : %d Tail : %d\n",h,t);
+
+    
+
+}
